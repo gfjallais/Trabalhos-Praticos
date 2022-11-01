bc0f8a53597e1ea986698d5361eee935d5f33c20
Gabriel Franco Jallais <80989996+gfjallais@users.noreply.github.com>
2022-11-01 00:42:26 -0300

    Add files via upload

---
+++ TP2-ALGORITMOS/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "rock.hpp"
+
+
+using namespace std;
+
+int main() {
+  
+  unsigned int qtd_amigos; // 1 <= qtd_amigos <= 50
+  unsigned int qtd_shows; // 1 <= qtd_shows <= 100000
+  float aux;
+  
+  while(cin >> qtd_amigos >> qtd_shows){
+
+    if(qtd_amigos == 0 && qtd_shows == 0) break;
+    
+    float shows[qtd_shows] = {0};
+
+    for(unsigned int i = 0; i < qtd_amigos; i++){
+      for(unsigned int j = 0; j < qtd_shows; j++){
+        cin >> aux;
+        shows[j] += aux;
+      }
+    }
+
+    max_subarray_sum_dq(shows, qtd_shows);
+
+  }
+  return 0;
+}
---
+++ TP2-ALGORITMOS/rock.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void max_subarray_sum_dq(float *shows, int size);
+float max_subsum_w_mid(float* shows, int *left, int *mid, int *right, float *aux);
+float max_subsum(float* shows, int *left, int *right, float *aux);
